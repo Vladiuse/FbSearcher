620a91f634fca309d4f67f2e84f53ec80582a4a8
vladiuse <vladiuse@gmail.com>
2023-09-26 15:58:45 +0300

    add new versipn of FbMainPage class

--- fb/parsers/fb_pages/fb_main_page.py
+++ fb/parsers/fb_pages/fb_main_page.py
@@ -1,11 +1,37 @@
+import os
+
 from bs4 import BeautifulSoup
+import re
+
 
 class FbPageError(Exception):
     def __init__(self, msg=''):
         self.msg = msg
 
 class FbMainPage:
+    URL = 'https://facebook.com/'
 
+    def __init__(self, html):
+        self.html = html
+        self.soup = BeautifulSoup(html, 'lxml')
+
+    def _is_register_page(self):
+        return '"ACCOUNT_ID":"0","USER_ID":"0"' in self.html
+
+    def _is_auth_page(self):
+        return bool(re.search('"ACCOUNT_ID":"\d{8,18}","USER_ID":"\d{8,18}",', self.html))
+
+    @property
+    def is_auth(self):
+        """Являеться страница страницей пользователя(залогиненого)"""
+        is_register_page = self._is_register_page()
+        is_auth_page = self._is_auth_page()
+        if is_register_page == is_auth_page:
+            raise FbPageError('Лог/разлог не определен')
+        return not is_register_page and is_auth_page
+
+
+class FbMainPageOld:
     URL = 'https://facebook.com/'
 
     def __init__(self, html):
@@ -24,6 +50,7 @@ class FbMainPage:
         if self.soup.find('div', {'id': 'splash-screen'}):
             return True
         return False
+
     @property
     def is_auth(self):
         """Являеться страница страницей пользователя(залогиненого)"""
@@ -33,9 +60,14 @@ class FbMainPage:
             raise TypeError('Значения не должны быть равны')
         return not is_register_page and is_auth_page
 
+
+
 if __name__ == '__main__':
-    path = '/home/vlad/html/error_main_fb_page.html'
-    with open(path) as file:
-        html = file.read()
-        page = FbMainPage(html)
-        print(page.is_auth)
+    dir_path = '/home/vlad/PycharmProjects/FbSearcher/fb/media/test_account_cookie_auth'
+    for file in os.listdir(dir_path):
+        path = os.path.join(dir_path, file)
+        with open(path) as file:
+            html = file.read()
+            page = FbMainPage(html)
+            print(file, page.is_auth)
+
