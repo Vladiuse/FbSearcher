9826c8ee91dfcdaea86a2d39354bd549fb4b4497
vladiuse <vladiuse@gmail.com>
2023-10-20 20:49:48 +0300

    add few funcs in fb_templates app

--- fb/fb_templates/models.py
+++ fb/fb_templates/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class FbTemplate(models.Model):
+    desc = models.CharField(
+        max_length=255,
+        blank=True
+    )
+    file = models.FileField(
+        upload_to='fb_templates',
+    )
+    prev = models.URLField(
+        blank=True,
+    )
--- fb/fb_templates/views.py
+++ fb/fb_templates/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import FbTemplate
 
 
 def index(request):
-    content = {}
+    content = {
+        'fb_templates': FbTemplate.objects.all()
+    }
     return render(request,'fb_templates/index.html', content)
 
