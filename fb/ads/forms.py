2dff4b49879c2566ce0dd78c0a3a8fd5517c09cd
vladiuse <vladiuse@gmail.com>
2024-01-30 01:23:00 +0300

    add DS check in DB exists from file name

--- fb/ads/forms.py
+++ fb/ads/forms.py
@@ -4,6 +4,7 @@ from django.core.validators import FileExtensionValidator
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from countries.models import Country
+from remote_pc.models import DS
 
 
 class FbLibCsvForm(forms.Form):
@@ -53,21 +54,33 @@ class TxtFileForm(forms.Form):
         if cleaned_data['add_in_stat']:
             not_match_file_names = []
             loaded_countries = set()
+            loaded_remote_pc = set()
             for file in self.files.getlist("txt_files"):
                 try:
-                    ds, country, other = file.name.split('_')
+                    ds_name, country, other = file.name.split('_')
                     loaded_countries.add(country.lower())
+                    loaded_remote_pc.add(ds_name.upper())
                 except ValueError:
                     not_match_file_names.append( file.name)
             if not_match_file_names:
                 raise ValidationError(
                     [ValidationError(_(f'Incorrect file name: {file_name}'), code='error') for file_name in not_match_file_names]
                 )
+
             countries_in_db = Country.objects.filter(pk__in=loaded_countries).values('pk')
             if len(countries_in_db) != len(loaded_countries):
                 if_bd_exists_codes = set([country['pk'] for country in countries_in_db])
                 incorrect_codes = loaded_countries - if_bd_exists_codes
                 raise ValidationError(
                     [ValidationError(_(f'Incorrect country iso code in file name: {incorrecr}!')) for incorrecr in incorrect_codes]
+                )
+
+            remote_pc_in_db = DS.objects.filter(name__in=loaded_remote_pc).values('name')
+            if len(remote_pc_in_db) != len(loaded_remote_pc):
+                pc_db_exists_names = set([pc['name'] for pc in remote_pc_in_db])
+                incorrect_pc_names = loaded_remote_pc - pc_db_exists_names
+                raise ValidationError(
+                    [ValidationError(_(f'Incorrect DS name: {incorrecr}!')) for incorrecr in
+                     incorrect_pc_names]
                 )
         return cleaned_data
