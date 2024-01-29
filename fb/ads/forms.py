1b31bda7b15beeb875dea86750ba70971bff5970
vladiuse <vladiuse@gmail.com>
2024-01-29 23:31:00 +0300

    add check country in file name if checbox add statt is ON

--- fb/ads/forms.py
+++ fb/ads/forms.py
@@ -1,5 +1,9 @@
+import re
 from django import forms
 from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from countries.models import Country
 
 
 class FbLibCsvForm(forms.Form):
@@ -33,8 +37,39 @@ class FbLibZipForm(forms.Form):
 
 
 class TxtFileForm(forms.Form):
+    """
+    Загруска групп из txt файла
+    """
     txt_files = forms.FileField(
         validators=[FileExtensionValidator(allowed_extensions=['txt']), ],
         widget=forms.FileInput(
             attrs={'class': "form-control"})
     )
+    add_in_stat = forms.BooleanField(required=False)
+
+    def clean(self):
+        """При созранении статистики должны быть указана страна в имени файла"""
+        cleaned_data = super().clean()
+        if cleaned_data['add_in_stat']:
+            not_match_file_names = []
+            loaded_countries = set()
+            for file in self.files.getlist("txt_files"):
+                try:
+                    ds, country, other = file.name.split('_')
+                    loaded_countries.add(country.lower())
+                except ValueError:
+                    not_match_file_names.append( file.name)
+            print(not_match_file_names)
+            if not_match_file_names:
+                raise ValidationError(
+                    [ValidationError(_(f'Incorrect file name: {file_name}'), code='error') for file_name in not_match_file_names]
+                )
+            print(loaded_countries)
+            countries_in_db = Country.objects.filter(pk__in=loaded_countries).values('pk')
+            if len(countries_in_db) != len(loaded_countries):
+                if_bd_exists_codes = set([country['pk'] for country in countries_in_db])
+                incorrect_codes = loaded_countries - if_bd_exists_codes
+                raise ValidationError(
+                    [ValidationError(_(f'Incorrect country iso code in file name: {incorrecr}!')) for incorrecr in incorrect_codes]
+                )
+        return cleaned_data
